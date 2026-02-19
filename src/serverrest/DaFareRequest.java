059dd469443e1e059abcde39adbb1276747258cb
Stefano Del Furia <stefano.delfuria@franchettisalviani.net>
2026-02-19 17:15:23 +0100

    Primo commit

---
+++ src/serverrest/DaFareRequest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverrest;
+
+/**
+ *
+ * @author delfo
+ */
+public class DaFareRequest {
+    
+    
+    // Costruttore vuoto necessario per GSON
+    public DaFareRequest() {
+    }
+    
+    // Costruttore con parametri  
+   
+    
+    // Getter
+   
+    
+    // Setter
+    
+
+    // ToString
+    
+}
