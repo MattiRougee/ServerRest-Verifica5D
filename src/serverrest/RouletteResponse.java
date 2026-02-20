d09d30f247f0c4eeced67ade0646fedada966621
Mattia Rossi <64710471+MattiRougee@users.noreply.github.com>
2026-02-20 09:25:00 +0100

    Request&Response

---
+++ src/serverrest/RouletteResponse.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverrest;
+
+/**
+ *
+ * @author delfo
+ */
+public class RouletteResponse {
+    
+    private String giocata;
+    private Integer numero;
+    
+    // Costruttore vuoto necessario per GSON
+    public RouletteResponse() {
+    }
+
+    public RouletteResponse(String giocata, Integer numero) {
+        this.giocata = giocata;
+        this.numero = numero;
+    }
+
+    public String getGiocata() {
+        return giocata;
+    }
+
+    public void setGiocata(String giocata) {
+        this.giocata = giocata;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+}
